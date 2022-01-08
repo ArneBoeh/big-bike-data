779a9208f8dc39e7f35abbc329122e612f9743d5
ArneBoeh <arneboehm@gmx.de>
2022-01-08 20:22:03 +0000

    Identify different CSV sources

--- app.js
+++ app.js
@@ -2,13 +2,28 @@ import fetch from 'node-fetch';
 
 const BIKE_DATA_QUERY = 'https://www.opengov-muenchen.de/api/3/action/package_search?q=raddauerzaehlstellen&rows=1000';
 
+const DATASET_TAGE_PATTERN  = /rad[0-9]{6}tage.*\.csv/;
+const DATASET_15MIN_PATTERN = /rad[0-9]{6}15min.*\.csv/;
+
+function loadCSV (url, table) {
+    console.log (table, url);
+}
+
+function loadResource(url) {
+    if (url.match(DATASET_TAGE_PATTERN)) {
+        loadCSV(url, 'tage');
+    }
+    if (url.match(DATASET_15MIN_PATTERN)) {
+        loadCSV(url, '15min');
+    }
+}
+
 fetch (BIKE_DATA_QUERY)
     .then(response => response.json())
     .then(json => {
         json.result.results.forEach(result => {
             result.resources.forEach(resource => {
-                console.log(resource.url);
+                loadResource(resource.url);
             });
         })
     });
-    
