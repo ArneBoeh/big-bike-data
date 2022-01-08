7c557a0b13519586b9a6cc959c12d1b5bcfc4888
ArneBoeh <arneboehm@gmx.de>
2022-01-08 18:07:57 +0000

    Query datasets

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-const message = 'Hello World'
+import fetch from 'node-fetch';
 
-console.log(message, 'from Gitpod!')
+const BIKE_DATA_QUERY = 'https://www.opengov-muenchen.de/api/3/action/package_search?q=raddauerzaehlstellen&rows=1000';
+
+fetch (BIKE_DATA_QUERY)
+    .then(response => response.json())
+    .then(json => {
+        console.log(json)}
+    );
