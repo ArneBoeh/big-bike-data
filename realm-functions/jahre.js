de582520409313921f5c69b885e95b14a3541608
ArneBoeh <arneboehm@gmx.de>
2022-01-23 06:51:03 +0000

    Query for Jahre

---
+++ realm-functions/jahre.js
@@ -0,0 +1,37 @@
+
+   
+exports = function() {
+
+    return context.services.get("mongodb-atlas").db("frequencies").collection("tage").aggregate([
+      {
+        $group: {
+          _id: { jahr: { $year: "$zeit" }, zaehlstelle: "$zaehlstelle" },
+          gesamt: { $sum: "$gesamt" }
+        }
+      },
+      {
+        $group: {
+          _id: "$_id.jahr",
+          gesamt: { $sum: "$gesamt" },
+          zaehlstellen: { $push: {
+            zaehlstelle: "$_id.zaehlstelle",
+            gesamt: "$gesamt"
+          }}
+        }
+      },
+      {
+        $project: {  
+          _id: 0,
+          jahr: "$_id",
+          gesamt: 1,
+          zaehlstellen: 1
+        }
+      },
+      {
+        $sort: {  
+          jahr: 1,
+        }
+      }
+    ]).toArray();
+  
+  };
