ef5f5d4528e37e2f8e6f79c729c135078a46c6d0
ArneBoeh <arneboehm@gmx.de>
2022-01-23 20:06:56 +0000

    More Queries

--- realm-functions/jahre.js
+++ realm-functions/jahre.js
@@ -2,36 +2,23 @@
    
 exports = function() {
 
-    return context.services.get("mongodb-atlas").db("MunichBikes").collection("tage").aggregate([
-      {
-        $group: {
-          _id: { jahr: { $year: "$zeit" }, zaehlstelle: "$zaehlstelle" },
-          gesamt: { $sum: "$gesamt" }
-        }
-      },
-      {
-        $group: {
-          _id: "$_id.jahr",
-          gesamt: { $sum: "$gesamt" },
-          zaehlstellen: { $push: {
-            zaehlstelle: "$_id.zaehlstelle",
-            gesamt: "$gesamt"
-          }}
-        }
-      },
-      {
-        $project: {  
-          _id: 0,
-          jahr: "$_id",
-          gesamt: 1,
-          zaehlstellen: 1
-        }
-      },
-      {
-        $sort: {  
-          jahr: 1,
-        }
+  return context.services.get("mongodb-atlas").db("MunichBikes").collection("tage").aggregate([
+    {
+      $group: {
+        _id: { $year: "$zeit" }
       }
-    ]).toArray();
-  
-  };
+    },
+    {
+      $project: {  
+        _id: 0,
+        jahr: "$_id",
+      }
+    },
+    {
+      $sort: {  
+        jahr: 1,
+      }
+    }
+  ]).toArray();
+
+};
