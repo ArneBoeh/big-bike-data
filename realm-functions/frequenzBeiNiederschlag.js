0ca5480d3539faf44f90648ca401102a24ce47ae
ArneBoeh <arneboehm@gmx.de>
2022-01-23 06:55:01 +0000

    More Queries

---
+++ realm-functions/frequenzBeiNiederschlag.js
@@ -0,0 +1,27 @@
+exports = function() {
+
+    return context.services.get("mongodb-atlas").db("frequencies").collection("tage").aggregate([
+      {
+        $group: {
+          _id: { tag: "$zeit"  },
+          frequenz: { $sum: "$gesamt" },
+          niederschlag: { $max: "$niederschlag" }
+        }
+      },
+      {
+        $group: {
+          _id: { niederschlag: { $cond : ["$niederschlag", "Niederschlag", "Kein Niederschlag" ] }  },
+          frequenz: { $sum: "$frequenz" }
+        }
+      },
+      {
+        $project: {  
+          _id: 0,
+          niederschlag: "$_id.niederschlag",
+          frequenz: 1,
+        }
+      }
+    ]).toArray();
+  
+  };
+  
