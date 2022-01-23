a5ee734cbabd1a73f6c46b817471715064cb475e
ArneBoeh <arneboehm@gmx.de>
2022-01-23 21:27:30 +0000

    Enpoint to collect tage ranges

---
+++ realm-functions/tage.js
@@ -0,0 +1,28 @@
+exports = function(query) {
+
+    var von = new Date(query.von);
+    var bis = new Date(query.bis);
+    var zaehlstelle = query.zaehlstelle;
+  
+    return context.services.get("mongodb-atlas").db("frequencies").collection("tage").aggregate([
+      {
+        $match: { 
+          zeit: { $gte: von, $lte: bis },
+          zaehlstelle: { $eq: zaehlstelle }
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          frequenz: '$gesamt',
+          tag: '$zeit'
+        }
+      },
+      {
+        $sort: {  
+          zeit: 1,
+        }
+      }
+    ]).toArray();
+  
+  };
