46ba15191c19be5e872a1cf303feaeb79c69160a
ArneBoeh <arneboehm@gmx.de>
2022-01-23 06:52:34 +0000

    More Queries

---
+++ realm-functions/frequenzProTemperaturUndNiederschlag.js
@@ -0,0 +1,28 @@
+exports = function() {
+
+    return context.services.get("mongodb-atlas").db("frequencies").collection("tage").aggregate([
+      {
+        $group: {
+          _id: { tag: "$zeit"  },
+          frequenz: { $sum: "$gesamt" },
+          niederschlag: { $max: "$niederschlag" },
+          temperatur: { $max: "$max-temp" }
+        }
+      },
+      {
+        $group: {
+          _id: { niederschlag: { $round : "$niederschlag" }, temperatur: { $round : "$temperatur" },  },
+          frequenz: { $sum: "$frequenz" }
+        }
+      },
+      {
+        $project: {  
+          _id: 0,
+          niederschlag: "$_id.niederschlag",
+          temperatur: "$_id.temperatur",
+          frequenz: 1,
+        }
+      }
+    ]).toArray();
+  
+  };
